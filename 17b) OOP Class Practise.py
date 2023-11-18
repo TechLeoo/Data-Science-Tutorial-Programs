4bca29826b90cf56e9d8e713f30849be5d3ed883
Onyiriuba Leonard <leonardleo110011@gmail.com>
2023-11-18 14:22:52 +0100

    Create Files

---
+++ 17b) OOP Class Practise.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 18 12:30:05 2023
+
+@author: lEO
+"""
+
+class Human:
+    def __init__(self, firstname, lastname, age, phone_number, height, weight, skin_color, eye_color, hair_style, body_build, shoe_size, fathers_name, mothers_name, friends, gender):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.phone_number = phone_number
+        self.height = height
+        self.weight = weight
+        self.skin_color = skin_color
+        self.eye_color = eye_color
+        self.hair_style = hair_style
+        self.body_build =  body_build   # Possible options ("Average Size", "Muscular", "Chubby", "Obese", "Skinny", "Petite")
+        self.shoe_size = shoe_size
+        self.fathers_name = fathers_name
+        self.mothers_name = mothers_name
+        self.friends = []
+        self.gender = gender
+        
+
+Leonard = Human(
+    firstname = "Leonard", 
+    lastname = "Onyiriuba", 
+    age, phone_number, height, weight, skin_color, eye_color, hair_style, body_build, shoe_size, fathers_name, mothers_name, friends, gender)
+        
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+  
+    
+
+
