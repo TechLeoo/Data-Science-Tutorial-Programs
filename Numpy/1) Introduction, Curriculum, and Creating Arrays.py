e63656d2619ce6bfedf43f2bf387cf425660d6c0
Onyiriuba Leonard <leonardleo110011@gmail.com>
2023-11-21 20:52:23 +0100

    Create

---
+++ Numpy/1) Introduction, Curriculum, and Creating Arrays.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 21 19:53:02 2023
+
+@author: lEO
+"""
+
+# Curriculum
+# ------> UNDERSTAND WHAT THE DOT OPERATOR DOES...
+# ------> UNDERSTAND WHAT THE SQAURE BRACKETS TRULY DO...
+# 1a) What is Numpy?
+# 1b) Understanding Numpy's data structure
+# 2) What are Dimensions and Arrays?
+# 3) How to create an Array?
+# 4a) How to create data with Numpy?
+# 4b) Knowing when to use np.random.seed() and why you are using it.
+# 5a) How to index and slice an Array?
+# 5b) Filtering values in an Array -------> VERY VERY INPORTANT
+# 6) What it means to SHAPE and RESHAPE an Array?
+# 7) Basic statistics with Numpy
+# 8) Dealing with missing values and NAN -------> VERY VERY IMPORTANT
+# 9) Some interesting Numpy Functions
+
+
+import numpy as np
+# CREATING AN ARRAY IN NUMPY
+# ----> Scalar (0 Dimensions) ---> Magnitude with No Directions
+scalar = np.array([13])
+scalar1 = np.array([0])
+scalar2 = np.array([89])
+
+# ----> Vector (1 Dimensional) ---> Magnitude and One Direction | A list.
+vector = np.array([13, 0, 89])
+vector1 = np.array([1, 2, 3, "Man", True])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
