aa28883aa53e7a778af8f6a2564fbbc642d439c9
Onyiriuba Leonard <leonardleo110011@gmail.com>
2023-11-23 21:40:22 +0100

    Updates

--- Numpy/2) Creating Data with Numpy.py
+++ Numpy/2) Creating Data with Numpy.py
@@ -51,38 +51,67 @@ b = np.array([32769], dtype = np.int16) # -32768 to 32767
     # d) np.empty_like()
     
     
-# ......... USING NP.ONES ............
-ones1 = np.ones(shape = (7, 3), dtype = np.float16,) # Matrix
-ones2 = np.ones(shape = (26,), dtype = np.int8,) # Vector
-ones3 = np.ones(shape = (3, 6, 2), dtype = np.int16) # Tensor
+# # ......... USING NP.ONES ............
+# ones1 = np.ones(shape = (7, 3), dtype = np.float16,) # Matrix
+# ones2 = np.ones(shape = (26,), dtype = np.int8,) # Vector
+# ones3 = np.ones(shape = (3, 6, 2), dtype = np.int16) # Tensor
 
-ones4 = np.ones_like(a = ones1) # Copies the shape
-ones5 = np.ones_like(ones2, np.float16)
-ones6 = np.ones_like(ones3, np.int32)
+# ones4 = np.ones_like(a = ones1) # Copies the shape
+# ones5 = np.ones_like(ones2, np.float16)
+# ones6 = np.ones_like(ones3, np.int32)
 
-# ......... USING NP.ZEROS ............
-zeros1 = np.zeros(shape = (10000, 2), dtype = np.float16,) # Matrix
-zeros2 = np.zeros(shape = (50000,), dtype = np.int8,) # Vector
-zeros3 = np.zeros(shape = (13, 6, 12), dtype = np.int16) # Tensor
+# # ......... USING NP.ZEROS ............
+# zeros1 = np.zeros(shape = (10000, 2), dtype = np.float16,) # Matrix
+# zeros2 = np.zeros(shape = (50000,), dtype = np.int8,) # Vector
+# zeros3 = np.zeros(shape = (13, 6, 12), dtype = np.int16) # Tensor
 
-zeros4 = np.zeros_like(a = zeros1) # Copies the shape
-zeros5 = np.zeros_like(ones2, np.float16)
-zeros6 = np.zeros_like(ones3, np.int32)
+# zeros4 = np.zeros_like(a = zeros1) # Copies the shape
+# zeros5 = np.zeros_like(ones2, np.float16)
+# zeros6 = np.zeros_like(ones3, np.int32)
 
-# ......... USING NP.FULL ............
-full1 = np.full(shape = (10, 2), dtype = np.int8, fill_value = 13) # Matrix
-full2 = np.full(shape = (50000,), dtype = np.int8, fill_value = 3) # Vector
-full3 = np.full(shape = (13, 6, 12), dtype = np.int16, fill_value = 91) # Tensor
+# # ......... USING NP.FULL ............
+# full1 = np.full(shape = (10, 2), dtype = np.int8, fill_value = 13) # Matrix
+# full2 = np.full(shape = (50000,), dtype = np.int8, fill_value = 3) # Vector
+# full3 = np.full(shape = (13, 6, 12), dtype = np.int16, fill_value = 91) # Tensor
 
-full4 = np.full_like(a = full1, fill_value = 13) # Copies the shape
-full5 = np.full_like(zeros2, fill_value = 13)
-full6 = np.full_like(zeros3, fill_value = 13)
+# full4 = np.full_like(a = full1, fill_value = 13) # Copies the shape
+# full5 = np.full_like(zeros2, fill_value = 13)
+# full6 = np.full_like(zeros3, fill_value = 13)
 
-# ......... USING NP.EMPTY ............
-empty1 = np.empty(shape = (250, 12), dtype = np.int8) # Matrix
-empty2 = np.empty(shape = (7600,), dtype = np.int8) # Vector
-empty3 = np.empty(shape = (6, 789, 2), dtype = np.int16) # Tensor
+# # ......... USING NP.EMPTY ............
+# empty1 = np.empty(shape = (250, 12), dtype = np.int8) # Matrix
+# empty2 = np.empty(shape = (7600,), dtype = np.int8) # Vector
+# empty3 = np.empty(shape = (6, 789, 2), dtype = np.int16) # Tensor
 
-empty4 = np.empty_like(empty1) # Copies THE SHAPE
-empty5 = np.empty_like(full2, np.float16)
-empty6 = np.empty_like(full3, np.int32)
+# empty4 = np.empty_like(empty1) # Copies THE SHAPE
+# empty5 = np.empty_like(full2, np.float16)
+# empty6 = np.empty_like(full3, np.int32)
+
+# # ......... USING NP.EMPTY ............
+# arange1 = np.arange(100, dtype = np.int8) # arange without stop and step
+# arange2 = np.arange(100, 1000, dtype = np.float32) # arange without step but we have start and stop
+# arange3 = np.arange(200, step = 2,  dtype = np.int8) # arange without stop but has step
+# arange4 = np.arange(20, 100, step = 5) # arange with start, stop, and step
+
+# ......... USING NP.RANDOM ............
+random_store = np.array(["UK", "USA", "Dubai", "France", "China", "Nigeria", "Ghana", "Germany", "Japan", "Egypt", "Bangladesh", "Maldives", "Santorini"])
+probabilities = [0.147, 0.027, 0.127, 0.007, 0.006, 0.257, 0.107, 0.017, 0.007, 0.007, 0.087, 0.097, 0.107]
+
+
+random1 = np.random.choice(random_store, size = 3, replace = False, p = probabilities)
+random2 = np.random.random_sample()
+
+np.random.shuffle(random_store)
+
+random4 = np.random.rand() # SAME AS np.random_sample()
+random5 = np.random.randint(low = 2, high = 10, size = (10,))
+# random6 = np.random.randn() # Not to be used in it's singular form. Must be cobimed with the Z score formula
+
+np.random.seed(0)
+
+# random11 = np.random.exponential() # BASED ON YOUR USE CASE
+random12 = np.random.normal()
+# random13 = np.random.binomial()
+# random14 = np.random.uniform()
+
+
