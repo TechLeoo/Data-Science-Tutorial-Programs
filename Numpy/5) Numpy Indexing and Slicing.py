b9599de8b76998b963af886f1f7ea890ca70862d
Onyiriuba Leonard <leonardleo110011@gmail.com>
2023-11-29 21:51:18 +0100

    Updates

--- Numpy/5) Numpy Indexing and Slicing.py
+++ Numpy/5) Numpy Indexing and Slicing.py
@@ -47,6 +47,92 @@ i = matrix_array[-5, :]
 j = matrix_array[:, -2]
 
 
+# -----> INDEXING <----- Indexing when used alone, gives you the exact location of an item in an object
+# Indexing in python starts at ZERO
+
+# VECTOR --> [ROW] for index
+# MATRIX --> [ROW, COLUMN] for index
+# TENSOR --> [MATRIX, ROW, COLUMN] for index
+
+
+# -----> SLICING <-----
+# VECTOR --> [:] for index
+# MATRIX --> [:, :] for index
+# TENSOR --> [:, :, :] for index
+
+
+# NOTE:
+    # 1) Index deals with only numbers. So above, the rows and column location will just be a number if you are indexing.
+    # 2) Slicing makes use of a number and a COLON (:).
+    
+
+
+
+
+# MORE EXAMPLES
+        # INDEXING
+# MATRIX --> [ROW, COLUMN] for index
+# VECTOR --> [ROW] for index
+
+result = matrix_array[5, 2]
+# result1 = matrix_array[3, 3] --> THIS WON'T WORK. COLUMN OUT OF BOUNDS
+result2 = matrix_array[4, 1]
+result3 = matrix_array[8, 2]
+result4 = vector_array[13]
+result5 = vector_array[8]
+result6 = vector_array[18]
+# result7 = vector_array[2, 3] --> ERROR
+result8 = matrix_array[-7, -2]
+result9 = matrix_array[-9, 2]
+result10 = matrix_array[7, -3]
+result11 = vector_array[-13]
+
+        # SLICING
+# VECTOR --> [:] for index
+# MATRIX --> [:, :] for index
+
+slice1 = vector_array[:]
+slice2 = vector_array[7:]
+slice3 = vector_array[0:]
+slice4 = vector_array[:0]
+slice5 = vector_array[:18]
+slice6 = vector_array[9:15]
+
+slice7 = vector_array[-3:]
+slice8 = vector_array[17:]
+
+slice2a = vector_array[-13:]
+slice3a = vector_array[-20:]
+slice4a = vector_array[:-20]
+slice5a = vector_array[:-2]
+slice6a = vector_array[-11:-5]
+slice6b = vector_array[-11:15]
+slice6c = vector_array[9:-5]
+
+np.random.seed(0)
+array = np.random.normal(loc = 35, scale = 15, size = (15, 6))
+matrix_slice = array[:10, :]
+matrix_slice1 = array[10:, :]
+matrix_slice2 = array[3:8, :]
+matrix_slice3 = array[:, 2:]
+matrix_slice4 = array[:, :2]
+matrix_slice5 = array[:, 0]
+matrix_slice6 = array[:, [0, 2]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
