37076f49a1cf236ff4baae27c8a16a64a0e5ce70
Onyiriuba Leonard <leonardleo110011@gmail.com>
2023-12-05 13:11:19 +0100

    Updates

--- File_Handling/1) Introduction_and_ Curriculum.py
+++ File_Handling/1) Introduction_and_ Curriculum.py
@@ -24,6 +24,39 @@ import pandas as pd
     # 1) np.genfromtxt()
     # 2) np.loadtxt()
 
+# DATA 1
 data1_numpy1 = np.genfromtxt("laliga21-22.csv", delimiter = ",", skip_header = 1, filling_values = 0)
 data1_numpy2 = np.loadtxt("laliga21-22.csv", delimiter = ",", skiprows = 1, usecols = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34])
 data1_pandas = pd.read_csv("laliga21-22.csv")
+data1_open = open("laliga21-22.csv", mode = "r",)
+data1_open = data1_open.read()
+
+# DATA 2
+# data2_numpy1 = np.genfromtxt("Personal_Info.txt", skip_header = 1, filling_values = 0) # FAILS US
+# data2_numpy2 = np.loadtxt("Personal_Info.txt", skiprows = 1, usecols = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]) # FAILS US
+# data2_pandas = pd.read_csv("Personal_Info.txt") # FAILS US
+data2_open = open("Personal_Info.txt", mode = "r",)
+data2_open = data2_open.read()
+
+# DATA 3
+# data3_numpy1 = np.genfromtxt("PharmDataset-230517-152700.xlsx", skip_header = 1, filling_values = 0) # FAILS US
+# data3_numpy2 = np.loadtxt("PharmDataset-230517-152700.xlsx", skiprows = 1, usecols = [0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34]) # FAILS US
+# data3_pandas1 = pd.read_csv("PharmDataset-230517-152700.xlsx") # FAILS US
+data3_pandas2 = pd.read_excel("PharmDataset-230517-152700.xlsx")
+# data3_open = open("PharmDataset-230517-152700.xlsx", mode = "r",) # FAILS US
+# data3_open = data3_open.read()
+
+# DATA 4
+data4_numpy1 = np.genfromtxt("Restaurant_Reviews.tsv", skip_header = 1, filling_values = 0, delimiter = "\t", dtype = str)
+data4_numpy2 = np.loadtxt("Restaurant_Reviews.tsv", dtype = str, usecols = [0, 1], delimiter = "\t", skiprows = 1) 
+data4_pandas1 = pd.read_csv("Restaurant_Reviews.tsv", delimiter = "\t")
+# data4_pandas2 = pd.read_excel("Restaurant_Reviews.tsv", ) # FAILS US
+data4_open = open("Restaurant_Reviews.tsv", mode = "r",) # FAILS US
+data4_open = data4_open.read()
+
+# DATA 5
+data5_numpy1 = np.genfromtxt("Students_Info.txt", skip_header = 1, filling_values = 0, delimiter = ";", dtype = str)
+data5_numpy2 = np.loadtxt("Students_Info.txt", dtype = str, delimiter = ";", skiprows = 1) 
+data5_pandas1 = pd.read_csv("Students_Info.txt", delimiter = ";")
+data5_open = open("Students_Info.txt", mode = "r",) 
+data5_open = data5_open.read()
---
+++ Pandas/1) Introduction and Curriculum.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  4 20:49:13 2023
+
+@author: lEO
+"""
+
+# WELCOME TO PANDAS
+# CURRICULUM
+# 1) What is pandas used for?
+# 2) Types of operations we do to get the data clean
+# 3) Step by step process on how to work through these operations
+# 4) Exploring some othr interesting PANDAS FUNCTIONS
+# 5) EDA with Pandas
+
+
+# -------------------------------------------------
+# 2) Pandas Operations for Data Cleaning:
+    # ---> Fixing Missing Values
+    # ---> Data Transformation
+    # ---> Data Scaling
