515b1831c8427a747cf7e90bfba9cc9d9cc0c359
Onyiriuba Leonard <leonardleo110011@gmail.com>
2023-12-06 20:02:38 +0100

    Updates

--- Pandas/2b) EDA with Pandas.py
+++ Pandas/2b) EDA with Pandas.py
@@ -18,6 +18,9 @@ warnings.filterwarnings("ignore")
 # (2) GET THE DATASET
 dataset = pd.read_csv("50_Startups.csv")
 
+# dataset = pd.DataFrame({"R&D Spend": [12, 13, 8, 2, 9, 18, 6], "Profit": [16700, 18000, 11500, 500, 13000, 25500, 8700]})
+# dataset = pd.DataFrame({"R&D Spend": [8, 6, 12, 18, 9, 2, 13], "Profit": [16700, 18000, 11500, 500, 13000, 25500, 8700]})
+
 # (3) EDA ---> Exploratory Data Analysis
 dataset.info()
 data_head = dataset.head()
@@ -34,4 +37,31 @@ data_histogram = dataset.hist(figsize = (15, 10), bins = 10)
 plt.figure(figsize = (15, 10))
 data_heatmap = sns.heatmap(data_correlation_matrix, cmap = "coolwarm", annot = True)
 
-# ---> DRAW THE SCATTER PLOT FOR THE CORRELATION MATRIX
+# # ---> DRAW THE SCATTER PLOT FOR THE CORRELATION MATRIX
+# # Plotting Correlation between R&D Spending and Profit
+# plt.figure(figsize = (15, 10)) # Draws the frame for the image
+# plt.scatter(dataset["R&D Spend"], dataset["Profit"], c = "green", s = 90) # Create a scatter plot
+# plt.title("Correlation between R&D Spend and Profit")
+# plt.xlabel("R&D Spend")
+# plt.ylabel("Profit")
+# plt.show() # Denotes the end of your graph and shows it
+
+# # Plotting Correlation between Administration and Profit
+# plt.figure(figsize = (15, 10)) # Draws the frame for the image
+# plt.scatter(dataset["Administration"], dataset["Profit"], c = "red", s = 90) # Create a scatter plot
+# plt.title("Correlation between Administration and Profit")
+# plt.xlabel("Administration")
+# plt.ylabel("Profit")
+# plt.show() # Denotes the end of your graph and shows it
+
+# # Plotting Correlation between Marketing Spend and Profit
+# plt.figure(figsize = (15, 10)) # Draws the frame for the image
+# plt.scatter(dataset["Marketing Spend"], dataset["Profit"], c = "blue", s = 90) # Create a scatter plot
+# plt.title("Correlation between Marketing Spend and Profit")
+# plt.xlabel("Marketing Spend")
+# plt.ylabel("Profit")
+# plt.show() # Denotes the end of your graph and shows it
+
+
+
+
